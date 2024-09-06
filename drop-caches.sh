3b7a56298ff7d1f1858d581cfe1f4fdafdb69e3b
Shreyan Sanyal <38560882+pneycho@users.noreply.github.com>
2024-09-06 17:14:17 +0530

    Update drop-caches.sh

--- drop-caches.sh
+++ drop-caches.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+#Name the script drop-caches.sh
+#Mark it as executable using chmod a+x drop-caches
+#Call it using sudo ./drop-caches
+#If you place the script in /usr/local/bin you can call it using sudo drop-caches
+
+
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 sync; echo 1 > /proc/sys/vm/drop_caches
 sync; echo 2 > /proc/sys/vm/drop_caches
